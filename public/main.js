44ec3f6b71557187ed2380f691e1e170eb8403e2
Pippin404 <Baconpippin@gmail.com>
2025-05-09 17:04:11 -0400

    Register functinality working! full front to backend

--- public/main.js
+++ public/main.js
@@ -13,3 +13,23 @@ export async function fetchData(route = '', data = {}, methodType) {
     throw await response.json();
   }
 }
+
+
+
+//local storage
+// local storage functions
+export function setCurrentUser(user) {
+  localStorage.setItem('user', JSON.stringify(user))
+}
+
+export function getCurrentUser() {
+  return JSON.parse(localStorage.getItem('user'))
+}
+// example accessing userId for second entity
+// let currentUser = getCurrentUser()
+// let userId = currentUser.userId
+
+export function removeCurrentUser() {
+  localStorage.removeItem('user')
+  window.location.href = "index.html"
+}
--- public/register.js
+++ public/register.js
@@ -1,20 +1,20 @@
+import { fetchData } from "./main.js"
 let loginForm = document.getElementById('register')
 loginForm.addEventListener('submit', registerFunction)
-import { fetchData } from "./main.js"
+
+
 
 function registerFunction(e) {
     e.preventDefault()
   
-    let fname = document.getElementById('fname').value
-    let lname = document.getElementById('lname').value
+    console.log("registerFunction called");
+
     let username = document.getElementById('username').value
     let email = document.getElementById('email').value
     let password = document.getElementById('password').value
   
     
     const user = {
-      fname: fname,
-      lname: lname,
       username: username,
       email: email,
       password: password
@@ -24,8 +24,6 @@ function registerFunction(e) {
     console.log(user)
 
     
-    document.getElementById('fname').value = ""
-    document.getElementById('lname').value = ""
     document.getElementById('username').value = ""
     document.getElementById('email').value = ""
     document.getElementById('password').value = ""
@@ -33,16 +31,18 @@ function registerFunction(e) {
 
 
     fetchData("/users/register", user, "POST")
-    .then(data => {
-      if(!data.message) {
-        console.log("User created")
-        console.log(data)
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      
-    })
+      .then(data => {
+        if(!data.message) {
+          console.log("User registered successfully")
+          setCurrentUser(data)
+          window.location.href = "home.html"
+        }
+      })
+      .catch(err => {
+        errorSection.innerText = `${err.message}`
+      })
+
+
 
 
 
